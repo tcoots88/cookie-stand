53b7d66a682cacf4d87cf5f36fe900e784089a2d
tcoots88 <travis.coots@yahoo.com>
2019-11-13 10:13:37 -0800

    added picture

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 'use strict';
-var time = ['6:00 am: ', '7:00 am: ', '8:00 am: ', '9:00 am: ', '10:00 am: ', '11:00 am: ', '12:00 pm: ', '1:00 pm: ', '2:00 pm: ', '3:00 pm: ', '4:00 pm: ', '5:00 pm: ', '6:00 pm: ', '7:00 pm: ', 'Total'];
+var time = ['6:00 am: ', '7:00 am: ', '8:00 am: ', '9:00 am: ', '10:00 am: ', '11:00 am: ', '12:00 pm: ', '1:00 pm: ', '2:00 pm: ', '3:00 pm: ', '4:00 pm: ', '5:00 pm: ', '6:00 pm: ', '7:00 pm: ', 'Total by Location'];
 var place = [];
 var container = document.getElementById('container');
 
@@ -11,7 +11,7 @@ function generateHeader() {
   }
 }
 function generateFooter() {
-  addElement('td', container, 'Total');
+  addElement('td', container, 'Hourly Total');
   for (var j = 0; j < time.length - 1; j++) {
     addElement('td', container, getTotalsForFooter(j));
   }
