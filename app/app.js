84337f74ec3e0fcf42be0f38b576142efae29500
tcoots88 <travis.coots@yahoo.com>
2019-11-13 09:26:42 -0800

    missed caily total column

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 'use strict';
-var time = ['6:00 am: ', '7:00 am: ', '8:00 am: ', '9:00 am: ', '10:00 am: ', '11:00 am: ', '12:00 pm: ', '1:00 pm: ', '2:00 pm: ', '3:00 pm: ', '4:00 pm: ', '5:00 pm: ', '6:00 pm: ', '7:00 pm: '];
+var time = ['6:00 am: ', '7:00 am: ', '8:00 am: ', '9:00 am: ', '10:00 am: ', '11:00 am: ', '12:00 pm: ', '1:00 pm: ', '2:00 pm: ', '3:00 pm: ', '4:00 pm: ', '5:00 pm: ', '6:00 pm: ', '7:00 pm: ', 'Total'];
 var place = [];
 var container = document.getElementById('container');
 
